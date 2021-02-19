9f3b4b719cdcb16106d85d5d375fcd01ef8a559b
Vazkuz <d.madalengoitia@pucp.edu.pe>
2021-02-19 00:33:12 -0500

    Added buttons and sprites
    Added buttons:
    - Quit button
    - Leave button
    
    Added card sprites

---
+++ 8 Locos - Repo/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.Disconnect();
+    }
+}
