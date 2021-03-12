1208400e44ae45e9b73d26768b185f82ac8719fe
d.madalengoitia <d.madalengoitia@pucp.edu.pe>
2021-03-12 01:51:17 -0500

    Added cards prefabs

---
+++ 8 Locos - Repo/Assets/Scripts/CardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+     
+    [SerializeField] public enum CardNumber {Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King};
+    [SerializeField] public enum CardSuit {Hearts /*Corazón*/, Clovers /*Tréboles*/, Tiles /*Diamante*/, Pikes /*Espada*/}; //Suit = palo
+
+    [SerializeField] public CardNumber cardNumber;
+    [SerializeField] public CardSuit cardSuit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 8 Locos - Repo/Assets/Scripts/GameController.cs
+++ 8 Locos - Repo/Assets/Scripts/GameController.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject deckCanvas;
+    [SerializeField] GameObject openDeckButton;
+
+    void Start()
+    {
+        deckCanvas.SetActive(false);
+        openDeckButton.SetActive(true);
+    }
+    public void ViewDeck()
+    {
+        deckCanvas.SetActive(true);
+        openDeckButton.SetActive(false);
+    }
+
+    public void CloseDeckOptions()
+    {
+        deckCanvas.SetActive(false);
+        openDeckButton.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
