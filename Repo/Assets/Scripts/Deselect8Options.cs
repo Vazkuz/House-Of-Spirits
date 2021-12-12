125d1dcef9f4432940e37ae8608f53c9a9f038ce
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2021-12-11 20:53:15 -0500

    Bug fixes

--- Repo/Assets/Scripts/Deselect8Options.cs
+++ Repo/Assets/Scripts/Deselect8Options.cs
@@ -8,6 +8,7 @@ public class Deselect8Options : MonoBehaviour, IDeselectHandler
     public void OnDeselect(BaseEventData eventData)
     {
         GameController.gameController.card8Options.SetActive(false);
+        GameController.gameController.ToggleCardOptions(false);
         GameController.gameController.cardSuitChosen = Card.CardSuit.NoColor;
     }
 }
