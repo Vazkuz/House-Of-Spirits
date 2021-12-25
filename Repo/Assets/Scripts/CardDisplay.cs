67751108fa1c1c3e2d93011650025fd1bbada8b5
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2021-12-24 22:35:26 -0500

    DisableCard bug fixed

--- Repo/Assets/Scripts/CardDisplay.cs
+++ Repo/Assets/Scripts/CardDisplay.cs
@@ -261,7 +261,7 @@ public class CardDisplay : MonoBehaviour
                 child.gameObject.SetActive(true);
                 child.localPosition = new Vector3(-((CardDisplay.cardDisplayInstance.maxCardsPerRow / 2 - (indexCard-indexCardOnLeft)) * CardDisplay.cardDisplayInstance.distanceBetweenCardsX), 0, 0);
                 child.gameObject.GetComponent<CardController>().positionInHand = indexCard-indexCardOnLeft;
-                // child.GetComponent<Animator>().Play("HoverOff_Card");
+                child.GetComponent<Animator>().Play("Idle_Card");
             }
             else
             {
